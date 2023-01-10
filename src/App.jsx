6b0f6c5ad8e2e0bdf3f392bfe7dcf6983eff9d3b
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2023-01-11 02:48:48 +0500

    Git Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from 'react-router-dom';
+import Home from './Pages/Home.jsx';
+import ProductDetails from './Pages/ProductDetails.jsx';
+import Sidebar from './Components/Sidebar.jsx';
+import Header from './Components/Header.jsx';
+import Footer from './Components/Footer.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ecommerce</h1>
-    </div>
-  );
-}
+const App = () => {
+
+    return (
+        <div className='overflow-hidden'>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/product/:id' element={<ProductDetails />} />
+                </Routes>
+                <Sidebar />
+                <Footer />
+            </Router>
+        </div>
+    )
+};
 
 export default App;
