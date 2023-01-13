fe76d85204ad321177fc79bd1fc74989f4fd7b80
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2023-01-14 02:52:36 +0500

    Git Init

--- src/Contexts/ProductContext.jsx
+++ src/Contexts/ProductContext.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
-const ProductContext = () => {
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        const fetchProducts = async () => {
+
+            const response = await fetch('https://fakestoreapi.com/products');
+
+            const data = await response.json();
+            console.log(data)
+            // setProducts(data);
+        };
+
+        fetchProducts();
+
+    }, []);
+
     return (
-        <div>
-
-        </div>
+        <ProductContext.Provider>
+            {children}
+        </ProductContext.Provider>
     )
 };
 
-export default ProductContext;
+export default ProductProvider;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductContext } from '../Contexts/ProductContext';
 
 const Home = () => {
+
+    const { products } = useContext(ProductContext);
+
+    const filteredProducts = products.filter(items => {
+        return (
+            items.category === "men's clothing" || items.category === "women's clothing"
+        );
+    });
+
+    // console.log(filteredProducts)
+
     return (
         <div>
-
+            <section>
+                <div className="container mx-auto">
+                    <div>
+                        {filteredProducts.map(product => {
+                            return <div>
+                                {product.title}
+                            </div>
+                        })}
+                    </div>
+                </div>
+            </section>
         </div>
     )
 };
